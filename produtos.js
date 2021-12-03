478f66e129fb55afd56669e6bc5f3660276daa1e
leonardo-m-pacheco <92610569+leonardo-m-pacheco@users.noreply.github.com>
2021-12-03 18:23:42 -0300

    .

--- produtos.js
+++ produtos.js
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = window.document.getElementsByClassName("mySlides fade");
+  var slides = window.document.getElementsByClassName("mySlides");
   var dots = window.document.getElementsByClassName("dot");
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
@@ -21,6 +21,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += " active";
 }
